e35dd4e7b0a346f11421bf9415e8d89084f37e4c
Arnaud Boussaer <ABSR@axi.be>
2025-02-07 16:15:16 +0100

    add extra reference to odata project in tests project

---
+++ OData2Linq/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace OData2Linq
+{
+    public class Program
+    {
+        static void Main()
+        {
+            //Class1 inst = new Class1();
+            //inst.Test();
+        }
+    }
+}
