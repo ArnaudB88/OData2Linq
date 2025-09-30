3ca6f52705297cd5fbb943b51204bc88814e4cc3
Arnaud Boussaer <ABSR@axi.be>
2025-09-30 13:14:04 +0200

    update message

--- test/OData2Linq.Tests/ODataTests.cs
+++ test/OData2Linq.Tests/ODataTests.cs
@@ -52,7 +52,7 @@
             Trace.WriteLine($"Private bytes after test run: {Math.Round(bytesInUse, 2)}MB ({MemoryUsageInMBStart}MB at start)(Iteration {iteration})");
 
             double increaseInMB = bytesInUse - MemoryUsageInMBStart.Value;
-            Assert.True(increaseInMB < 15, $"Memory usage should not increase by more than 10MB (increase={increaseInMB})");
+            Assert.True(increaseInMB < 15, $"Memory usage should not increase by more than 15MB (increase={increaseInMB})");
         }
     }
 }
